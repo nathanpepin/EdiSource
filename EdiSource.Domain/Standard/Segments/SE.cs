b96bae62207ae032ab54de129fb3f02ee7e2c525
nathanpepin <46303125+nathanpepin@users.noreply.github.com>
2024-10-14 13:26:52 -0400

    Bla

--- EdiSource.Domain/Standard/Segments/SE.cs
+++ EdiSource.Domain/Standard/Segments/SE.cs
@@ -1,5 +1,13 @@
+using EdiSource.Domain.Identifiers;
 using EdiSource.Domain.Segments;
+using EdiSource.Domain.Standard.Loops;
+using EdiSource.Loops;
 
 namespace EdiSource.Domain.Standard.Segments;
 
-public sealed class SE : Segment;
+public sealed class SE : Segment, ISegment<GenericTransactionSet>, ISegmentIdentifier<SE>
+{
+    
+    public GenericTransactionSet? Parent { get; }
+    public static (string Primary, string? Secondary) EdiId { get; } = ("SE", null);
+}
--- EdiSource.Domain/Standard/Segments/ST.cs
+++ EdiSource.Domain/Standard/Segments/ST.cs
@@ -1,5 +1,12 @@
+using EdiSource.Domain.Identifiers;
 using EdiSource.Domain.Segments;
+using EdiSource.Domain.Standard.Loops;
+using EdiSource.Loops;
 
 namespace EdiSource.Domain.Standard.Segments;
 
-public sealed class ST : Segment;
+public sealed class ST : Segment, ISegment<GenericTransactionSet>, ISegmentIdentifier<ST>
+{
+    public GenericTransactionSet? Parent { get; }
+    public static (string Primary, string? Secondary) EdiId { get; } = ("ST", null);
+}
